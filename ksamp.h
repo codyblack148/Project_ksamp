1e5bb505ac0cd05fa60f957992d130a46c86f4a5
Cody Black <codyblack@Codys-MacBook-Pro-3.local>
2018-02-10 16:41:31 -0600

    Initial commit: prototypes and constants.

---
+++ ksamp.h
@@ -0,0 +1,18 @@
+#ifndef _KSAMP_H_
+#define _KSAMP_H_
+
+#define MAX_BUF_LEN 500 //should be big enough?
+#define TRUE 1
+#define debug 0
+
+int main(int, char **);
+void usage(void);
+void defaultUse(void);
+void lInfo(int, int);
+void sInfo(void);
+void usage(void);
+char *getSubStr(const char *, const char *);
+char *getEntry(int, const char *, const char *);
+
+
+#endif
